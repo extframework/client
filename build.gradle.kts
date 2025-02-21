b494050792a838ff7a76081accaae73786ed6f8f
durganmcbroom <durganmcbroom@gmail.com>
2025-02-21 11:40:45 -0800

    Windows bug fixes and version bump.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,13 @@ plugins {
     java
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("dev.extframework.common") version "1.0.49"
+    id("dev.extframework.common") version "1.0.50"
 
     id("me.champeau.mrjar") version "0.1.1"
 }
 
 group = "dev.extframework"
-version = "1.1.2-BETA"
+version = "1.1.3-BETA"
 
 repositories {
     mavenCentral()
